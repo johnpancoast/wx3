aca391a2b1b1bee84e25c0a23d5de5beda5882e0
John Pancoast <shideon@gmail.com>
2013-09-15 00:00:35 -0700

    toml config suffix changed

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	cfgFile := os.Getenv("WX3_CONFIG");
 	if cfgFile == "" {
-		cfgFile = "/etc/wx3.conf"
+		cfgFile = "/etc/wx3.toml"
 	}
 
 	cfg  := toml.Parser{}.ParseFile(cfgFile)
